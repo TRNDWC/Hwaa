355da4fd67357f179d5d4b7e4d8dbb85f30f0dda
Trần Anh Đức <117843173+TRNDWC@users.noreply.github.com>
2024-09-29 22:45:35 +0700

    resource adjustment

--- app/src/main/java/com/example/hwaa/util/ui/HwaaToolBar.kt
+++ app/src/main/java/com/example/hwaa/util/ui/HwaaToolBar.kt
@@ -3,14 +3,18 @@ package com.example.hwaa.util.ui
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
-import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
-import androidx.databinding.DataBindingUtil
+import androidx.viewbinding.ViewBinding
 import com.example.hwaa.R
-import com.example.hwaa.databinding.TbMainActivyBinding
+import com.example.hwaa.databinding.TbBookFragmentBinding
+import com.example.hwaa.databinding.TbDetailPostFragmentBinding
+import com.example.hwaa.databinding.TbForumFragmentBinding
+import com.example.hwaa.databinding.TbLessonFragmentBinding
+import com.google.android.material.appbar.CollapsingToolbarLayout
 
 interface HwaaToolBarCallBack {
-    fun backIconClickListener()
+    fun backBookClickListener()
+    fun backPostClickListener()
 }
 
 class HwaaToolBar @JvmOverloads constructor(
@@ -19,20 +23,68 @@ class HwaaToolBar @JvmOverloads constructor(
     defStyleAttr: Int = 0
 ) : Toolbar(context, attrs, defStyleAttr) {
 
-    private val binding: TbMainActivyBinding = DataBindingUtil.inflate(
-        LayoutInflater.from(context),
-        R.layout.tb_main_activy,
-        this,
-        true
-    )
+    private var binding: ViewBinding? = null
+    var callBack: HwaaToolBarCallBack? = null
 
-    fun getBinding() = binding
+    fun inflateToolBarLayout(layoutId: Int, collapsingToolbarLayout: CollapsingToolbarLayout) {
+        removeAllViews()
+        binding = when (layoutId) {
+            R.layout.tb_lesson_fragment -> {
+                val binding =
+                    TbLessonFragmentBinding.inflate(LayoutInflater.from(context), this, true)
+                setListenerForLesson(binding)
+                binding
+            }
 
-    var backIconClickListener: HwaaToolBarCallBack? = null
+            R.layout.tb_forum_fragment -> {
+                val binding =
+                    TbForumFragmentBinding.inflate(LayoutInflater.from(context), this, true)
+                setListenerForForum(binding)
+                binding
+            }
 
-    init {
+            R.layout.tb_book_fragment -> {
+                val binding =
+                    TbBookFragmentBinding.inflate(LayoutInflater.from(context), this, true)
+                setListenerForBook(binding)
+                binding
+            }
+
+            R.layout.tb_detail_post_fragment -> {
+                val binding =
+                    TbDetailPostFragmentBinding.inflate(LayoutInflater.from(context), this, true)
+                setListenerForDetailPost(binding)
+                binding
+            }
+
+            else -> null
+        }
+
+        post {
+            collapsingToolbarLayout.requestLayout()
+            collapsingToolbarLayout.invalidate()
+        }
+    }
+
+    private fun setListenerForLesson(binding: TbLessonFragmentBinding) {
         binding.ibBack.setOnClickListener {
-            backIconClickListener?.backIconClickListener()
+            callBack?.backBookClickListener()
+        }
+    }
+
+    private fun setListenerForForum(binding: TbForumFragmentBinding) {
+//        binding.ivBack.setOnClickListener {
+//            callBack?.backIconClickListener()
+//        }
+    }
+
+    private fun setListenerForBook(binding: TbBookFragmentBinding) {
+
+    }
+
+    private fun setListenerForDetailPost(binding: TbDetailPostFragmentBinding) {
+        binding.ibBack.setOnClickListener {
+            callBack?.backPostClickListener()
         }
     }
 }
